1c023ca075d537f8b1173f31eb55d61369046244
Saifsaad1 <Saifsaad65a@gmail.com>
2023-04-30 22:18:51 +0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -7,20 +7,24 @@ let aboutArrow = document.querySelector('.about-arrow');
 let aboutHiddenText = document.querySelector('.about-hidden-text');
 let aboutArrow2 = document.querySelector('.about-arrow-2');
 let aboutHiddenText2 = document.querySelector('.about-hidden-text-2');
+let a241 = document.querySelector('.a241');
 
 hum.onclick = function(){
     nav.classList.toggle('navbar-list-show');
     hum1.classList.toggle('hum-line-1-rotate');
     hum2.classList.toggle('hum-line-2-rotate');
-    hum3.classList.toggle('hum-line-3-opacity')
+    hum3.classList.toggle('hum-line-3-opacity');
 }
 aboutArrow.onclick = function(){
     aboutHiddenText.classList.toggle('show-about-hidden-text')
 
+
 }
 aboutArrow2.onclick = function(){
     aboutHiddenText2.classList.toggle('show-about-hidden-text')
+
 }
+
 
 
 
